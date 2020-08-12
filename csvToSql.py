bb5885ba534aa330fe9c5610c43a5f61a75bf0df
anthofio <anthony.fioravanti1@gmail.com>
2020-08-12 19:31:23 +0200

    write sql file

--- csvToSql.py
+++ csvToSql.py
@@ -4,6 +4,7 @@ import sys
 with open('MOCK_DATA.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    sqlFile = open(r"sqlQuery.sql","w+") 
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
@@ -11,7 +12,14 @@ with open('MOCK_DATA.csv') as csv_file:
             sql_adresse = f'Insert into ADRESSE ({row[2]},{row[3]})'
             line_count += 1
         else:
-            print(f'{sql_etablissement} VALUES (SEQ.NEXTVAL,{row[0]}, {row[1]})')
-            print(f'{sql_adresse} VALUES (SEQ.NEXTVAL,{row[2]}, {row[3]})')
+            str1 = f'{sql_etablissement} VALUES (SEQ.NEXTVAL,{row[0]}, {row[1]});'
+            str2 = f'{sql_adresse} VALUES (SEQ.NEXTVAL,{row[2]}, {row[3]});'
+            print(str1)
+            print(str2)
+            L = ['\n',str1, str2,'\n']
+            sqlFile.writelines(L)
+            
             line_count += 1
     print(f'Processed {line_count} lines.')
+    sqlFile.write('\nCOMMIT;')
+    sqlFile.close
